5b0fabe65d00bc42663c518492c8ab85c13ddefb
sonderzhang <102024958+sonderzhang@users.noreply.github.com>
2023-07-24 11:11:12 +0800

    Add files via upload

---
+++ files/ModelsForGraph.py
@@ -0,0 +1,380 @@
+import dgl.nn.pytorch as dglnn
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+import dgl.function as fn
+from dgl.nn.functional import edge_softmax
+import dgl
+import torch
+from dgl.nn.pytorch import GATConv
+import numpy as np 
+
+
+
+
+class SEHGTLayer(nn.Module):
+    def __init__(self,
+                 in_dim,
+                 out_dim,
+                 node_dict,
+                 edge_dict,
+                 n_heads,
+                 dropout = 0.2,
+                 use_norm = False,
+                 reduction = 16):
+        super(SEHGTLayer, self).__init__()
+
+        self.in_dim        = in_dim
+        self.out_dim       = out_dim
+        self.node_dict     = node_dict
+        self.edge_dict     = edge_dict
+        self.num_types     = len(node_dict)
+        self.num_relations = len(edge_dict)
+        self.total_rel     = self.num_types * self.num_relations * self.num_types
+        self.n_heads       = n_heads
+        self.d_k           = out_dim // n_heads
+        self.sqrt_dk       = math.sqrt(self.d_k)
+        self.att           = None
+
+        self.k_linears   = nn.ModuleList()
+        self.q_linears   = nn.ModuleList()
+        self.v_linears   = nn.ModuleList()
+        self.a_linears   = nn.ModuleList()
+        self.norms       = nn.ModuleList()
+        self.use_norm    = use_norm
+
+
+        ####  SE BLOCK   ####
+        channel = 116
+        self.reduction = reduction
+        self.selayer = nn.Sequential(
+            nn.Linear(channel,channel//self.reduction,bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(channel//reduction,channel,bias=False),
+            nn.Sigmoid()            
+        )
+        ###################
+
+        for t in range(self.num_types):
+            self.k_linears.append(nn.Linear(in_dim,   out_dim))
+            self.q_linears.append(nn.Linear(in_dim,   out_dim))
+            self.v_linears.append(nn.Linear(in_dim,   out_dim))
+            self.a_linears.append(nn.Linear(out_dim,  out_dim))
+            if use_norm:
+                self.norms.append(nn.LayerNorm(out_dim))
+
+        self.relation_pri   = nn.Parameter(torch.ones(self.num_relations, self.n_heads))
+        self.relation_att   = nn.Parameter(torch.Tensor(self.num_relations, n_heads, self.d_k, self.d_k))
+        self.relation_msg   = nn.Parameter(torch.Tensor(self.num_relations, n_heads, self.d_k, self.d_k))
+        self.skip           = nn.Parameter(torch.ones(self.num_types))
+        self.drop           = nn.Dropout(dropout)
+        self.relation_wei   = nn.Parameter(torch.ones(self.num_relations))
+
+        nn.init.xavier_uniform_(self.relation_att)
+        nn.init.xavier_uniform_(self.relation_msg)
+
+    def forward(self, G, h):
+        with G.local_scope():
+            node_dict, edge_dict = self.node_dict, self.edge_dict
+            func = {}
+            for srctype, etype, dsttype in G.canonical_etypes:
+                sub_graph = G[srctype, etype, dsttype]
+
+                k_linear = self.k_linears[node_dict[srctype]]
+                v_linear = self.v_linears[node_dict[srctype]]
+                q_linear = self.q_linears[node_dict[dsttype]]
+
+                k = k_linear(h[srctype]).view(-1, self.n_heads, self.d_k)
+                v = v_linear(h[srctype]).view(-1, self.n_heads, self.d_k)
+                q = q_linear(h[dsttype]).view(-1, self.n_heads, self.d_k)
+
+                e_id = self.edge_dict[etype]
+
+                relation_att = self.relation_att[e_id]#n_heads*d_k*d_k
+                relation_pri = self.relation_pri[e_id]#n_heads
+                relation_msg = self.relation_msg[e_id]#n_heads*d_k*d_k
+
+                k = torch.einsum("bij,ijk->bik", k, relation_att)
+                #|V|*n_heads*d_k  x  n_heads*d_k*d_k = |v|*n_heads*d_k
+                v = torch.einsum("bij,ijk->bik", v, relation_msg)
+                # |v|*n_heads*dim
+
+                sub_graph.srcdata['k'] = k
+                sub_graph.dstdata['q'] = q
+                sub_graph.srcdata['v_%d' % e_id] = v * F.softmax(self.relation_wei,dim=0)[e_id]
+
+                sub_graph.apply_edges(fn.v_dot_u('q', 'k', 't'))
+                attn_score = sub_graph.edata.pop('t').sum(-1) * relation_pri / self.sqrt_dk
+                attn_score = edge_softmax(sub_graph, attn_score, norm_by='dst')
+
+                sub_graph.edata['t'] = attn_score.unsqueeze(-1)
+                func[etype]= (fn.u_mul_e('v_%d' % e_id, 't', 'm'), fn.sum('m', 't'))
+
+            G.multi_update_all(func, cross_reducer = 'mean')
+
+
+            new_h = {}
+            for ntype in G.ntypes:
+
+                n_id = node_dict[ntype]
+                alpha = torch.sigmoid(self.skip[n_id])
+                
+                t = G.nodes[ntype].data['t'].view(-1, self.out_dim)
+                temp = torch.mean(torch.exp(t),dim=1).view(-1,116)
+                temp = self.selayer(temp).view(-1,1)
+                trans_out = self.drop(self.a_linears[n_id](t))*temp
+                
+                trans_out = trans_out * alpha + h[ntype] * (1-alpha)
+                # For differnet kinds of nodes, give different weights in resducial connection
+                if self.use_norm:
+                    new_h[ntype] = self.norms[n_id](trans_out)
+                else:
+                    new_h[ntype] = trans_out
+            return new_h
+
+class SEHGT(nn.Module):
+    def __init__(self, node_dict, edge_dict, n_inp, n_hid, n_out, n_layers, n_heads, use_norm = True):
+        super(SEHGT, self).__init__()
+        self.node_dict = node_dict
+        self.edge_dict = edge_dict
+        self.gcs = nn.ModuleList()
+        self.n_inp = n_inp
+        self.n_hid = n_hid
+        self.n_out = n_out
+        self.n_layers = n_layers
+        self.adapt_ws  = nn.ModuleList()
+        for t in range(len(node_dict)):
+            self.adapt_ws.append(nn.Linear(n_inp,   n_hid))
+        for _ in range(n_layers):
+            self.gcs.append(SEHGTLayer(n_hid, n_hid, node_dict, edge_dict, n_heads, use_norm = use_norm))
+        self.out = nn.Sequential(nn.Linear(n_hid,n_hid),nn.ReLU(),nn.Linear(n_hid, n_out))
+
+    def forward(self, G):
+        h = {}
+        for ntype in G.ntypes:
+            n_id = self.node_dict[ntype]
+            h[ntype] = F.gelu(self.adapt_ws[n_id](G.nodes[ntype].data['feat']))
+        for i in range(self.n_layers):
+            h = self.gcs[i](G, h)
+        with G.local_scope():
+            if len(G.ntypes) == 1:
+                G.ndata['h'] = h[G.ntypes[0]]
+            else:
+                G.ndata['h'] = h
+            for ntype in G.ntypes:# with only one node type 
+                hg = dgl.mean_nodes(G, 'h', ntype=ntype)
+            return self.out(hg)
+
+
+class HGTLayer(nn.Module):
+    def __init__(self,
+                 in_dim,
+                 out_dim,
+                 node_dict,
+                 edge_dict,
+                 n_heads,
+                 dropout = 0.2,
+                 use_norm = False):
+        super(HGTLayer, self).__init__()
+
+        self.in_dim        = in_dim
+        self.out_dim       = out_dim
+        self.node_dict     = node_dict
+        self.edge_dict     = edge_dict
+        self.num_types     = len(node_dict)
+        self.num_relations = len(edge_dict)
+        self.total_rel     = self.num_types * self.num_relations * self.num_types
+        self.n_heads       = n_heads
+        self.d_k           = out_dim // n_heads
+        self.sqrt_dk       = math.sqrt(self.d_k)
+        self.att           = None
+
+        self.k_linears   = nn.ModuleList()
+        self.q_linears   = nn.ModuleList()
+        self.v_linears   = nn.ModuleList()
+        self.a_linears   = nn.ModuleList()
+        self.norms       = nn.ModuleList()
+        self.use_norm    = use_norm
+
+        for t in range(self.num_types):
+            self.k_linears.append(nn.Linear(in_dim,   out_dim))
+            self.q_linears.append(nn.Linear(in_dim,   out_dim))
+            self.v_linears.append(nn.Linear(in_dim,   out_dim))
+            self.a_linears.append(nn.Linear(out_dim,  out_dim))
+            if use_norm:
+                self.norms.append(nn.LayerNorm(out_dim))
+
+        self.relation_pri   = nn.Parameter(torch.ones(self.num_relations, self.n_heads))
+        self.relation_att   = nn.Parameter(torch.Tensor(self.num_relations, n_heads, self.d_k, self.d_k))
+        self.relation_msg   = nn.Parameter(torch.Tensor(self.num_relations, n_heads, self.d_k, self.d_k))
+        self.skip           = nn.Parameter(torch.ones(self.num_types))
+        self.drop           = nn.Dropout(dropout)
+
+        nn.init.xavier_uniform_(self.relation_att)
+        nn.init.xavier_uniform_(self.relation_msg)
+
+    def forward(self, G, h):
+        with G.local_scope():
+            
+            node_dict, edge_dict = self.node_dict, self.edge_dict
+            for srctype, etype, dsttype in G.canonical_etypes:
+                
+                sub_graph = G[srctype, etype, dsttype]
+
+                k_linear = self.k_linears[node_dict[srctype]]
+                
+                v_linear = self.v_linears[node_dict[srctype]]
+                q_linear = self.q_linears[node_dict[dsttype]]
+
+                k = k_linear(h[srctype]).view(-1, self.n_heads, self.d_k)
+                
+                v = v_linear(h[srctype]).view(-1, self.n_heads, self.d_k)
+                q = q_linear(h[dsttype]).view(-1, self.n_heads, self.d_k)
+
+                e_id = self.edge_dict[etype]
+
+                relation_att = self.relation_att[e_id]#n_heads*d_k*d_k
+                relation_pri = self.relation_pri[e_id]#n_heads
+                relation_msg = self.relation_msg[e_id]#n_heads*d_k*d_k
+
+                k = torch.einsum("bij,ijk->bik", k, relation_att)
+                #|V|*n_heads*d_k  x  n_heads*d_k*d_k = |v|*n_heads*d_k
+                v = torch.einsum("bij,ijk->bik", v, relation_msg)
+
+                sub_graph.srcdata['k'] = k
+                sub_graph.dstdata['q'] = q
+                sub_graph.srcdata['v_%d' % e_id] = v
+
+                sub_graph.apply_edges(fn.v_dot_u('q', 'k', 't'))
+                attn_score = sub_graph.edata.pop('t').sum(-1) * relation_pri / self.sqrt_dk
+                attn_score = edge_softmax(sub_graph, attn_score, norm_by='dst')
+
+                sub_graph.edata['t'] = attn_score.unsqueeze(-1)
+
+            G.multi_update_all({etype : (fn.u_mul_e('v_%d' % e_id, 't', 'm'), fn.sum('m', 't')) \
+                                for etype, e_id in edge_dict.items()}, cross_reducer = 'mean')
+
+            new_h = {}
+            for ntype in G.ntypes:
+                '''
+                    Step 3: Target-specific Aggregation
+                    x = norm( W[node_type] * gelu( Agg(x) ) + x )
+                '''
+                n_id = node_dict[ntype]
+                alpha = torch.sigmoid(self.skip[n_id])
+                t = G.nodes[ntype].data['t'].view(-1, self.out_dim)
+                trans_out = self.drop(self.a_linears[n_id](t))
+                trans_out = trans_out * alpha + h[ntype] * (1-alpha)
+                if self.use_norm:
+                    new_h[ntype] = self.norms[n_id](trans_out)
+                else:
+                    new_h[ntype] = trans_out
+            return new_h
+
+class HGT(nn.Module):
+    def __init__(self, node_dict, edge_dict, n_inp, n_hid, n_out, n_layers, n_heads, use_norm = True):
+        super(HGT, self).__init__()
+        self.node_dict = node_dict
+        self.edge_dict = edge_dict
+        self.gcs = nn.ModuleList()
+        self.n_inp = n_inp
+        self.n_hid = n_hid
+        self.n_out = n_out
+        self.n_layers = n_layers
+        self.adapt_ws  = nn.ModuleList()
+        for t in range(len(node_dict)):
+            self.adapt_ws.append(nn.Linear(n_inp,   n_hid))
+        for _ in range(n_layers):
+            self.gcs.append(HGTLayer(n_hid, n_hid, node_dict, edge_dict, n_heads, use_norm = use_norm))
+        self.out = nn.Sequential(nn.Linear(n_hid,n_hid),nn.ReLU(),nn.Linear(n_hid, n_out))
+
+    def forward(self, G):
+        h = {}
+        for ntype in G.ntypes:
+            n_id = self.node_dict[ntype]
+            h[ntype] = F.gelu(self.adapt_ws[n_id](G.nodes[ntype].data['feat']))
+            
+        for i in range(self.n_layers):
+            h = self.gcs[i](G, h)
+        with G.local_scope():
+            if len(G.ntypes) == 1:
+                G.ndata['h'] = h[G.ntypes[0]]
+            else:
+                G.ndata['h'] = h
+            for ntype in G.ntypes:# with only one node type 
+                hg = dgl.mean_nodes(G, 'h', ntype=ntype)
+            return self.out(hg)
+
+
+class SemanticAttention(nn.Module):                     
+    def __init__(self, in_size, hidden_size=128):
+        super(SemanticAttention, self).__init__()
+
+        self.project = nn.Sequential(
+            nn.Linear(in_size, hidden_size),
+            nn.Tanh(),   #激活函数tanh
+            nn.Linear(hidden_size, 1, bias=False)
+        )
+
+    def forward(self, z):
+        w = self.project(z).mean(0)                    # (M, 1)
+        beta = torch.softmax(w, dim=0)                 # (M, 1)  
+        beta = beta.expand((z.shape[0],) + beta.shape) # (N, M, 1)
+
+        return (beta * z).sum(1)                       # (N, D * K)
+
+class HANLayer(nn.Module):
+
+    def __init__(self, meta_paths, in_size, out_size, layer_num_heads, dropout):
+        super(HANLayer, self).__init__()
+
+        # One GAT layer for each meta path based adjacency matrix
+        self.gat_layers = nn.ModuleList()
+        for i in range(len(meta_paths)):
+            self.gat_layers.append(GATConv(in_size, out_size, layer_num_heads,
+                                           dropout, dropout, activation=F.elu,
+                                           allow_zero_in_degree=True))
+        self.semantic_attention = SemanticAttention(in_size=out_size * layer_num_heads)
+        self.meta_paths = list(tuple(meta_path) for meta_path in meta_paths)
+
+        self._cached_graph = None
+        self._cached_coalesced_graph = {}
+
+    def forward(self, g, h):
+        semantic_embeddings = []
+
+        if self._cached_graph is None or self._cached_graph is not g:
+            self._cached_graph = g
+            self._cached_coalesced_graph.clear()
+            for meta_path in self.meta_paths:
+                self._cached_coalesced_graph[meta_path] = dgl.metapath_reachable_graph(
+                        g, meta_path)
+
+        for i, meta_path in enumerate(self.meta_paths):
+            new_g = self._cached_coalesced_graph[meta_path]
+            semantic_embeddings.append(self.gat_layers[i](new_g, h).flatten(1))
+        semantic_embeddings = torch.stack(semantic_embeddings, dim=1)                  # (N, M, D * K)
+
+        return self.semantic_attention(semantic_embeddings)                            # (N, D * K)
+
+class HAN(nn.Module):
+    def __init__(self, meta_paths, in_size, hidden_size, out_size, num_heads, dropout):
+        super(HAN, self).__init__()
+
+        self.layers = nn.ModuleList()
+        self.layers.append(HANLayer(meta_paths, in_size, hidden_size, num_heads[0], dropout))
+        for l in range(1, len(num_heads)):
+            self.layers.append(HANLayer(meta_paths, hidden_size * num_heads[l-1],
+                                        hidden_size, num_heads[l], dropout))
+        self.predict = nn.Linear(hidden_size * num_heads[-1], out_size)
+
+    def forward(self, g):
+        h=g.ndata['feat']
+        for gnn in self.layers:
+            h = gnn(g, h)
+        with g.local_scope():
+            g.ndata['h'] = h
+            hg=0
+            for ntype in g.ntypes:# with only one node type 
+                hg = hg + dgl.mean_nodes(g, 'h', ntype=ntype)
+            return self.predict(hg)
